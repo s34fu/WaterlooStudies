29dc89835f7551878b5f397a6e3d2593f2bb4526
Jaco <j49ye@uwaterloo.ca>
2021-01-13 21:27:17 +0800

    Changes

--- src/Backend/Database/RequiredCoursesByFaculty.js
+++ src/Backend/Database/RequiredCoursesByFaculty.js
@@ -2,20 +2,51 @@ const Enums = require('../Enums');
 
 module.exports = [
 	{
-		programName: Enums.ProgramEnum.MGTE,
+		programName: Enums.ProgramEnum.AE,
 		courseGroups: [
-			{
-				name: Enums.CourseGroupEnum.LIST_1,
-				total: 6
-			},
 			{
 				name: Enums.CourseGroupEnum.CSE_LIST_A,
 				total: 1
+			},						
+			{
+				name: Enums.CourseGroupEnum.CSE_LIST_C,
+				total: 1
 			},
 			{
-				name: Enums.CourseGroupEnum.FREE_ELEC,
+				name: Enums.CourseGroupEnum.TECH_LIST_A,
+				total: 3
+			},
+			{
+				name: Enums.CourseGroupEnum.TECH_LIST_B,
 				total: 2
-			}
+			},
+			{
+				name: Enums.CourseGroupEnum.TECH_LIST_D,
+				total: 1
+			},
+		]
+	},
+	{
+		programName: Enums.ProgramEnum.BME,
+		courseGroups: [
+			{
+				name: Enums.CourseGroupEnum.CEAB,
+				total: 4
+			},		
+			{
+				name: Enums.CourseGroupEnum.FREE_RANGE,
+				total: 3,
+				acceptable: [
+					Enums.CourseGroupEnum.CSE_LIST_A,
+					Enums.CourseGroupEnum.CSE_LIST_B,
+					Enums.CourseGroupEnum.CSE_LIST_C,
+					Enums.CourseGroupEnum.CSE_LIST_D
+				]
+			},
+			{
+				name: Enums.CourseGroupEnum.TE,
+				total: 6
+			},				
 		]
 	},
 	{
@@ -102,15 +133,94 @@ module.exports = [
 				total: 1
 			},
 			{
+				name: Enums.CourseGroupEnum.TE,
+				total: 8
+			},	
+		]
+	},
+	{
+		programName: Enums.ProgramEnum.ECE,
+		courseGroups: [
+			{
+				name: Enums.CourseGroupEnum.CSE_LIST_C,
+				total: 2
+			},		
+			{
 				name: Enums.CourseGroupEnum.FREE_RANGE,
-				total: 8,
+				total: 1,
+				acceptable: [
+					Enums.CourseGroupEnum.CSE_LIST_A,
+					Enums.CourseGroupEnum.CSE_LIST_C,
+					Enums.CourseGroupEnum.CSE_LIST_D
+				]
+			},
+			{
+				name: Enums.CourseGroupEnum.ETHICS,
+				total: 1
+			},	
+			{
+				name: Enums.CourseGroupEnum.NSE,
+				total: 2
+			},	
+			{
+				name: Enums.CourseGroupEnum.TE,
+				total: 8
+			},	
+		]
+	},
+	{
+		programName: Enums.ProgramEnum.ENVE,
+		courseGroups: [
+			{
+				name: Enums.CourseGroupEnum.CSE_LIST_C,
+				total: 2
+			},		
+			{
+				name: Enums.CourseGroupEnum.TECH_LIST_A,
+				total: 4
+			},		
+			{
+				name: Enums.CourseGroupEnum.FREE_RANGE,
+				total: 3,
 				acceptable: [
 					Enums.CourseGroupEnum.TECH_LIST_A,
 					Enums.CourseGroupEnum.TECH_LIST_B,
-					Enums.CourseGroupEnum.TECH_LIST_C,
-					Enums.CourseGroupEnum.TECH_LIST_D
 				]
+			},
+		]
+	},
+	{
+		programName: Enums.ProgramEnum.GEOE,
+		courseGroups: [
+			{
+				name: Enums.CourseGroupEnum.CSE_LIST_A,
+				total: 1
 			},	
+			{
+				name: Enums.CourseGroupEnum.CSE_LIST_C,
+				total: 2
+			},	
+			{
+				name: Enums.CourseGroupEnum.TE,
+				total: 6
+			},	
+		]
+	},
+	{
+		programName: Enums.ProgramEnum.MGTE,
+		courseGroups: [
+			{
+				name: Enums.CourseGroupEnum.LIST_1,
+				total: 6
+			},
+			{
+				name: Enums.CourseGroupEnum.CSE_LIST_A,
+				total: 1
+			},
+			{
+				name: Enums.CourseGroupEnum.FREE_ELEC,
+				total: 2
+			}
 		]
 	},
 	{
@@ -129,9 +239,91 @@ module.exports = [
 				total: 2
 			},
 			{
+				name: Enums.CourseGroupEnum.TE,
+				total: 7
+			},	
+			{
 				name: Enums.CourseGroupEnum.FREE_ELEC,
 				total: 1
 			}
 		]
-	}
+	},
+	{
+		programName: Enums.ProgramEnum.MTE,
+		courseGroups: [
+			{
+				name: Enums.CourseGroupEnum.TE,
+				total: 5
+			},	
+		]
+	},
+	{
+		programName: Enums.ProgramEnum.NE,
+		courseGroups: [
+			{
+				name: Enums.CourseGroupEnum.TE,
+				total: 8
+			},	
+		]
+	},
+	{
+		programName: Enums.ProgramEnum.SE,
+		courseGroups: [
+			{
+				name: Enums.CourseGroupEnum.CSLIST,
+				total: 1
+			},	
+			{
+				name: Enums.CourseGroupEnum.ECELIST,
+				total: 1
+			},	
+			{
+				name: Enums.CourseGroupEnum.FREE_RANGE,
+				total: 1,
+				acceptable: [
+					Enums.CourseGroupEnum.CSLIST,
+					Enums.CourseGroupEnum.ECELIST,
+				]
+			},
+			{
+				name: Enums.CourseGroupEnum.SCE,
+				total: 2
+			},	
+			{
+				name: Enums.CourseGroupEnum.LE,
+				total: 3
+			},	
+			{
+				name: Enums.CourseGroupEnum.WKRPT,
+				total: 3
+			},	
+			{
+				name: Enums.CourseGroupEnum.PD,
+				total: 5
+			},
+		]
+	},
+	{
+		programName: Enums.ProgramEnum.SYDE,
+		courseGroups: [
+			{
+				name: Enums.CourseGroupEnum.CEAB,
+				total: 2
+			},	
+			{
+				name: Enums.CourseGroupEnum.FREE_RANGE,
+				total: 4,
+				acceptable: [
+					Enums.CourseGroupEnum.CSE_LIST_A,
+					Enums.CourseGroupEnum.CSE_LIST_B,
+					Enums.CourseGroupEnum.CSE_LIST_C,
+					Enums.CourseGroupEnum.CSE_LIST_D,
+				]
+			},
+			{
+				name: Enums.CourseGroupEnum.TE,
+				total: 6
+			},
+		]
+	},
 ];
--- src/Backend/Enums.js
+++ src/Backend/Enums.js
@@ -1,9 +1,18 @@
 const ProgramEnum = {
-	MGTE: 'Management Engineering',
+	AE: 'Architectural Engineering',
+	BME: 'Biomedical Engineering',
 	CHE: 'Chemical Engineering',
 	CIVE: 'Civil Engineering',
 	CE: 'Computer Engineering',
-	ME: 'Mechanical Engineering'
+	ECE: 'Electrical Engineering',
+	ENVE: 'Environmental Engineering',
+	GEOE: 'Geological Engineering',
+	MGTE: 'Management Engineering',
+	ME: 'Mechanical Engineering',
+	MTE: 'Mechatronics Engineering',
+	NE: 'Nanotechnology Engineering',
+	SE: 'Software Engineering',
+	SYDE: 'System Design Engineering'
 };
 
 const FacultyEnum = {
@@ -20,7 +29,15 @@ const CourseGroupEnum = {
 	TECH_LIST_B: 'TECH List B',
 	TECH_LIST_C: 'TECH List C',
 	TECH_LIST_D: 'TECH List D',
+	TE: 'Technical Electives',
 	NSE: 'Natural Science Electives',
+	CSLIST: 'CS Courses',
+	ECELIST: 'ECE Courses',
+	SCE: 'Science Electives',
+	LE: 'Linkage Electives',
+	WKRPT: 'Work-term Reports',
+	PD: 'Professional Development Courses',
+	CEAB: 'Canadian Engineering Accreditation Board Requirements',
 	ETHICS: 'Ethics Requirement',
 	FREE_ELEC: 'Free Elective',
 	FREE_RANGE: 'Range of Groups'
