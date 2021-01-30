ae07f327f6790453cc7cdb5b3e5cc8bdfaad09d7
Jaco <j49ye@uwaterloo.ca>
2021-01-30 15:32:51 +0800

    16-20

--- src/Backend/Database/AcademicYears.js
+++ src/Backend/Database/AcademicYears.js
@@ -4,5 +4,21 @@ module.exports = [
     {
         code: 1617,
         name: Enums.AcademicYearsEnum[1617]
+    },
+    {
+        code: 1718,
+        name: Enums.AcademicYearsEnum[1718]
+    },
+    {
+        code: 1819,
+        name: Enums.AcademicYearsEnum[1819]
+    },
+    {
+        code: 1920,
+        name: Enums.AcademicYearsEnum[1920]
+    },
+    {
+        code: 2021,
+        name: Enums.AcademicYearsEnum[2021]
     }
 ];
